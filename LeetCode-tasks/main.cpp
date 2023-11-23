54529a8907af05df12f060042436adfeccfb11ca
Seredenko-V <seredenko.spb@gmail.com>
2023-11-23 11:07:38 +0300

    Remove Duplicates from Sorted Array

--- LeetCode-tasks/main.cpp
+++ LeetCode-tasks/main.cpp
@@ -5,7 +5,25 @@ using namespace std;
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-
+        int unique_pos = 0;
+        int size = static_cast<int>(nums.size());
+        for (int i = 0; i < size - 1; ++i) {
+            while (nums[i] == nums[i + 1]) {
+                ++i;
+                if (i == size - 1) {
+                    break;
+                }
+            }
+            if (i == size - 1 || nums[i] > nums[i + 1]) {
+                break;
+            }
+            for (int j = i; j < size; ++j) {
+                nums[unique_pos + j - i] = nums[j];
+            }
+            i = unique_pos++;
+        }
+        nums.resize(++unique_pos); // в меньшую сторону за O(1)
+        return unique_pos;
     }
 };
 
