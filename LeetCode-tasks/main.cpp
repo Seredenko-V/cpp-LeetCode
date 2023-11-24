aaec7d8acea86f0130f98bddb6151f2cb5b6e635
Seredenko-V <seredenko.spb@gmail.com>
2023-11-24 19:50:26 +0300

    Contains Duplicate with use std::sort

--- LeetCode-tasks/main.cpp
+++ LeetCode-tasks/main.cpp
@@ -1,47 +1,20 @@
 #include <vector>
 #include <cassert>
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
-// https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/646/
-// решение на месте, без копирования вектора
-
-uint32_t GetGCD(int first, int second) noexcept {
-    if (first == 0 || second == 0) {
-        return 1;
-    }
-    if (first < second) {
-        swap(first, second);
-    }
-    while (first % second != 0) {
-        first = first % second;
-        swap(first, second);
-    }
-    return std::abs(second);
-}
-
-void rotate(vector<int>& nums, int k) {
-    size_t size = nums.size();
-    k = k % size;
-    if (nums.empty() || k == 0) {
-        return;
-    }
-    for (int start = 0; start < GetGCD(size, k); ++start) {
-        int index = start;
-        int prev = nums[start];
-        while (true) {
-            int new_index = (index + k) % size;
-            if (new_index == start) {
-                nums[new_index] = prev;
-                break;
-            }
-            int current = nums[new_index];
-            nums[new_index] = prev;
-            index = new_index;
-            prev = current;
+//https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/578/
+// с использованием std::sort
+bool containsDuplicate(vector<int>& nums) {
+    sort(nums.begin(), nums.end());
+    for (size_t i = 0; i < nums.size() - 1; ++i) {
+        if (nums[i] == nums[i + 1]) {
+            return true;
         }
     }
+    return false;
 }
 
 ostream& operator<<(ostream& out, const vector<int>& vec) {
@@ -53,53 +26,20 @@ ostream& operator<<(ostream& out, const vector<int>& vec) {
 
 void Test() {
     {
-        constexpr int k = 3;
         vector<int> nums{1,2,3,4,5,6,7};
-        vector<int> expected{5,6,7,1,2,3,4};
-        rotate(nums, k);
-        assert(nums == expected);
+        assert(!containsDuplicate(nums));
     }{
-        vector<int> nums{1,2,3,4,5,6,7};
-        vector<int> expected = nums;
-        int k = nums.size();
-        rotate(nums, k);
-        assert(nums == expected);
-    }{
-        constexpr int k = 2;
         vector<int> nums{-1,-100,3,99};
-        vector<int> expected{3,99,-1,-100};
-        rotate(nums, k);
-        assert(nums == expected);
+        assert(!containsDuplicate(nums));
     }{
-        constexpr int k = 0;
-        vector<int> nums{25,15,8,23,0};
-        vector<int> expected = nums;
-        rotate(nums, k);
-        assert(nums == expected);
+        vector<int> nums{25,15,8,23,0,8};
+        assert(containsDuplicate(nums));
     }{
-        constexpr int k = 2;
         vector<int> nums{-1};
-        vector<int> expected = nums;
-        rotate(nums, k);
-        assert(nums == expected);
+        assert(!containsDuplicate(nums));
     }{
-        constexpr int k = 3;
-        vector<int> nums{1,2,3,4,5,6};
-        vector<int> expected{4,5,6,1,2,3};
-        rotate(nums, k);
-        assert(nums == expected);
-    }{
-        constexpr int k = 1;
-        vector<int> nums{1,2,3};
-        vector<int> expected{3,1,2};
-        rotate(nums, k);
-        assert(nums == expected);
-    }{
-        constexpr int k = 4;
-        vector<int> nums{1,2,3,4,5,6};
-        vector<int> expected{3,4,5,6,1,2};
-        rotate(nums, k);
-        assert(nums == expected);
+        vector<int> nums{1,1,1,1,1,1};
+        assert(containsDuplicate(nums));
     }
 }
 
