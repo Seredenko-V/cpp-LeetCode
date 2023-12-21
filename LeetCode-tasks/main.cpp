0aa84e2710baeabc32219f5b33d7fceb3e5b6b1e
Seredenko-V <seredenko.spb@gmail.com>
2023-12-21 10:45:24 +0300

    Первый уникальный символ в строке. Не эффективное решение

--- LeetCode-tasks/main.cpp
+++ LeetCode-tasks/main.cpp
@@ -1,72 +1,40 @@
 #include <cassert>
 #include <iostream>
-#include <limits>
 #include <string>
-#include <algorithm>
-#include <cmath>
+#include <map>
 
 using namespace std;
 
-// Reverse Integer
-// https://leetcode.com/explore/interview/card/top-interview-questions-easy/127/strings/880/
+// First Unique Character in a String
+// https://leetcode.com/explore/interview/card/top-interview-questions-easy/127/strings/881/
 /******************************************************
-* Нужно развернуть цифры числа, которое записано в виде строки.
-* Если изменение числа приводит к выходу значения за пределы
-* диапазона 32-битных целых чисел со знаком [-2^31, 2^31 - 1],
-* нужно вернуть 0.
+* Дана строка s, найдите в ней первый неповторяющийся символ и верните его индекс.
+* Если он не существует, верните -1.
 *******************************************************/
 
-// Сложность: O(N)
-// Runtime: 5 ms - runtime beats 13.72 % of cpp submissions.
-// Memory Usage: 6.5  MB - memory usage beats ??? % of cpp submissions.
-int reverse(int x) {
-    if (x <= numeric_limits<int>::min() || x > numeric_limits<int>::max()) {
-        return 0;
+// Сложность: O(2*N)
+// Runtime: 65 ms - runtime beats 5.10 % of cpp submissions.
+// Memory Usage: 11.2  MB - memory usage beats 17.10 % of cpp submissions.
+int firstUniqChar(string s) {
+    map<char, pair<int, int>> elems;
+    for (size_t i = 0; i < s.size(); ++i) {
+        elems[s[i]].first = i;
+        ++elems[s[i]].second;
     }
-    int64_t result = 0;
-    int num_digits = 0;
-    for (int i = 1; num_digits != 9; ++i) {
-        const int kCurrentDigit = pow(10, i);
-        if (x % kCurrentDigit != x) {
-            ++num_digits;
-        } else {
-            break;
+    int pos_first_unique = -1;
+    for (const pair<const char, pair<int, int>>& elem : elems) {
+        const pair<int, int>& stat = elem.second;
+        if (stat.second == 1 && (stat.first < pos_first_unique || pos_first_unique == -1)) {
+            pos_first_unique = stat.first;
         }
     }
-    if (x < 0) {
-        for (int i = 1; i <= num_digits + 1; ++i) {
-            const int64_t kCurrentDigit = pow(10, i);
-            int64_t tmp_value = x % kCurrentDigit;
-            tmp_value /= pow(10, i - 1);
-            tmp_value *= pow(10, num_digits - i + 1);
-            result -= tmp_value;
-        }
-    } else {
-        for (int i = 1; i <= num_digits + 1; ++i) {
-            const int64_t kCurrentDigit = pow(10, i);
-            int64_t tmp_value = x % kCurrentDigit;
-            tmp_value /= pow(10, i - 1);
-            tmp_value *= pow(10, num_digits - i + 1);
-            result += tmp_value;
-        }
-    }
-    if (x < 0) {
-        result = -result;
-    }
-    // переполнение
-    if (result > numeric_limits<int>::max() || result < numeric_limits<int>::min()) {
-        return 0;
-    }
-    return static_cast<int>(result);
+    return pos_first_unique;
 }
 
 void Tests() {
-    assert(reverse(123) == 321);
-    assert(reverse(-123) == -321);
-    assert(reverse(120) == 21);
-    assert(reverse(132000) == 231);
-    assert(reverse(numeric_limits<int>::min()) == 0);
-    assert(reverse(1'534'236'469) == 0);
+    assert(firstUniqChar("leetcode"s) == 0);
+    assert(firstUniqChar("loveleetcode"s) == 2);
+    assert(firstUniqChar("aabb"s) == -1);
     cerr << "Tests has passed\n"s;
 }
 
