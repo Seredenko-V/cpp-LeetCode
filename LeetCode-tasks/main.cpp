d3c15a17fe87b77d250941bae897d2df4a2f5090
Seredenko-V <seredenko.spb@gmail.com>
2024-01-24 09:51:29 +0300

    Количество перестановок. C из n по k

--- LeetCode-tasks/main.cpp
+++ LeetCode-tasks/main.cpp
@@ -1,13 +1,15 @@
 #include <cassert>
 #include <iostream>
 #include <cstdint>
+#include <stdexcept>
+#include <string>
 
 using namespace std;
 
-// Определить количество возможных перестановок
-// https://new.contest.yandex.ru/48556/problem?id=215/2023_04_06/xAmHQ1PYv2
+// Определить количество сочетаний
+// https://new.contest.yandex.ru/48556/problem?id=215/2023_04_06/yrA534pVxW
 
-uint64_t Factorial(uint8_t n) {
+uint64_t Factorial(int n) {
     uint64_t fact = 1u;
     for (uint8_t i = 2; i <= n; ++i) {
         fact *= i;
@@ -15,18 +17,30 @@ uint64_t Factorial(uint8_t n) {
     return fact;
 }
 
+uint64_t NumCombinations(int n, int k) {
+    if (n < k) {
+        return 0;
+    }
+    if (n > 7 || k > 7) {
+        return 0;
+    }
+    if (n < 1 || k < 1) {
+        return 0;
+    }
+    return Factorial(n) / (Factorial(k) * Factorial(n - k));
+}
+
 void Tests() {
-    assert(Factorial(0) == 1);
-    assert(Factorial(1) == 1);
-    assert(Factorial(3) == 6);
-    assert(Factorial(5) == 120);
-    cerr << "Tests has passed\n"s;
+    assert(NumCombinations(3, 2) == 3);
+    assert(NumCombinations(7, 5) == 21);
+    assert(NumCombinations(1, 1) == 1);
+    cerr << "Tests passed\n"s;
 }
 
 int main() {
     Tests();
-    int n = 0;
-    cin >> n;
-    cout << Factorial(n);
+    int n = 0, k = 0;
+    cin >> n >> k;
+    cout << NumCombinations(n, k);
     return 0;
 }
