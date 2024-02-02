06752531ce89330a91bcff743b2326871af907e1
Seredenko-V <seredenko.spb@gmail.com>
2024-02-02 13:08:45 +0300

    Динамическое программирование. Камни 2. Решение через формулу

--- LeetCode-tasks/main.cpp
+++ LeetCode-tasks/main.cpp
@@ -16,36 +16,6 @@ using namespace std;
 // Когда камень забрали, он выходит из игры. Побеждает игрок, который заберет последний камень. Первый ход за вами.
 // Вы и ваш оппонент играете оптимально.
 
-ostream& operator<<(ostream& out, const vector<vector<bool>>& vec) {
-    for (const vector<bool>& str : vec) {
-        for (bool elem : str) {
-            out << elem << ' ';
-        }
-        out << endl;
-    }
-    out << "=========================\n";
-    return out;
-}
-
-vector<vector<bool>> CreateMatrix(int n, int m) {
-    vector<vector<bool>> matrix(n, vector<bool>(m, true));
-    for (int i = 0; i < n; i += 3) {
-        matrix[i][0] = false;
-    }
-    for (int j = 0; j < m; j += 3) {
-        matrix[0][j] = false;
-    }
-
-    for (int i = 1; i < n; ++i) {
-        for (int j = 1; j < m; ++j) {
-            if (matrix[i - 1][j - 1] == false) {
-                matrix[i][j] = false;
-            }
-        }
-    }
-    return matrix;
-}
-
 bool IsWinRocks(int n, int m) {
     if (n < 1 || n > 1000) {
         return false;
@@ -53,33 +23,11 @@ bool IsWinRocks(int n, int m) {
     if (m < 1 || m > 1000) {
         return false;
     }
-    vector<vector<bool>> matrix = CreateMatrix(n, m);
-    return matrix.back().back();
+    return abs(n - m) % 3;
 }
 
 namespace tests {
-    void TestCreateMatrix() {
-        {
-            vector<vector<bool>> expected_matrix{
-                {0, 1},
-                {1, 0}
-            };
-            vector<vector<bool>> matrix = CreateMatrix(2, 2);
-            assert(expected_matrix == matrix);
-        }{
-            vector<vector<bool>> expected_matrix{
-                {0, 1, 1},
-                {1, 0, 1},
-                {1, 1, 0},
-            };
-            vector<vector<bool>> matrix = CreateMatrix(3, 3);
-            assert(expected_matrix == matrix);
-        }
-        cerr << "TestCreateMatrix passed\n";
-    }
-
     void RunAllTest() {
-        TestCreateMatrix();
         assert(!IsWinRocks(4, 4));
         assert(IsWinRocks(7, 5));
         assert(IsWinRocks(17, 72));
