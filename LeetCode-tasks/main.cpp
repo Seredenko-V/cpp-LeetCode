327c6976f24880e88d8834ccb6443a976f4fd39f
Seredenko-V <seredenko.spb@gmail.com>
2024-02-02 09:19:37 +0300

    Динамическое программирование. Игра Камни. Решение через проверку на четность n и m

--- LeetCode-tasks/main.cpp
+++ LeetCode-tasks/main.cpp
@@ -7,118 +7,35 @@
 
 using namespace std;
 
-// Бронирование переговорки
-// https://new.contest.yandex.ru/48557/problem?id=215/2023_04_06/RdGbbmsLQn
-// Задано n интервалов. Требуется найти максимальное количество взаимно непересекающихся интервалов.
-// Два интервала пересекаются, если они имеют хотя бы одну общую точку.
+// Камни
+// https://new.contest.yandex.ru/48558/problem?id=215/2023_04_06/lX20wNIqZg
+// Вы играете в игру <<Камни>>: игру для двух игроков с двумя наборами камней по n и m штук.
+// С каждым ходом один игрок может взять один камень (из любого набора) или два камня (по одному из обоих).
+// Когда камень забрали, он выходит из игры. Побеждает игрок, который заберет последний камень. Первый ход за вами.
+// Вы и ваш оппонент играете оптимально.
 
-// ================================= АЛГОРИТМ РЕШЕНИЯ =================================
-// 1) Считываем все диапазоны в вектор, т.к. количество диапазонов известно заранее.
-// 2) Сортируем диапазоны по времени завершения отрезка, удаляем дубликаты.
-// 3) Ищем первый диапазон, время начала которого меньше, чем время завершения текущего диапазона.
-//    Используется алгоритм двух указателей.
-
-vector<pair<int, int>> ReadRanges(istream& in) {
-    int num_ranges = 0;
-    in >> num_ranges;
-    vector<pair<int, int>> ranges(num_ranges);
-    for (int i = 0; i < num_ranges; ++i) {
-        in >> ranges[i].first >> ranges[i].second;
-        if (ranges[i].first < 1 || ranges[i].first > ranges[i].second || ranges[i].second > 50) {
-            return {};
-        }
+bool IsWinRocks(int n, int m) {
+    if (n < 1 || n > 10) {
+        return false;
     }
-    return ranges;
+    if (m < 1 || m > 10) {
+        return false;
+    }
+    return n % 2 || m % 2;
 }
 
-void PrepareRanges(vector<pair<int, int>>& ranges) {
-    // сортировка по времени завершения отрезка (по возрастанию)
-    sort(ranges.begin(), ranges.end(), [](const pair<int, int>& lhs, const pair<int, int>& rhs) {
-        return lhs.second < rhs.second;
-    });
-    vector<pair<int, int>>::const_iterator last_unique = unique(ranges.begin(), ranges.end());
-    ranges.erase(last_unique, ranges.cend());
+void Test() {
+    assert(!IsWinRocks(10, 2));
+    assert(!IsWinRocks(6, 8));
+    assert(IsWinRocks(4, 5));
+    assert(IsWinRocks(3, 9));
+    cerr << "Test passed\n";
 }
-
-uint32_t GetCountDisjointRanges(const vector<pair<int, int>>& ranges) {
-    uint32_t num_disjoint_ranges = 1;
-    uint32_t current_range = 0;
-    for (size_t i = 1; i < ranges.size(); ++i) {
-        // ищем первый диапазон, время начала которого меньше, чем время завершения текущего диапазона
-        if (ranges[current_range].second < ranges[i].first) {
-            ++num_disjoint_ranges;
-            current_range = i;
-        }
-    }
-    return num_disjoint_ranges;
-}
-
-namespace tests {
-    void TestReadRanges() {
-        istringstream input{
-            "5\n"
-            "1 3\n"
-            "1 3\n"
-            "2 3\n"
-            "4 5\n"
-            "4 5\n"
-        };
-        vector<pair<int, int>> expected_ranges{{1, 3}, {1, 3}, {2, 3}, {4, 5}, {4, 5}};
-        vector<pair<int, int>> ranges = ReadRanges(input);
-        assert(expected_ranges == ranges);
-        cerr << "TestReadRanges passed\n";
-    }
-
-    void TestPrepareRanges() {
-        {
-            vector<pair<int, int>> expected_ranges{{1, 3}, {2, 3}, {4, 5}};
-            vector<pair<int, int>> ranges = expected_ranges;
-            PrepareRanges(ranges);
-            assert(ranges == expected_ranges);
-        }{
-            vector<pair<int, int>> expected_ranges{{1, 3}, {2, 3}, {4, 5}};
-            vector<pair<int, int>> ranges{{1, 3}, {1, 3}, {2, 3}, {4, 5}, {4, 5}};
-            PrepareRanges(ranges);
-            assert(ranges == expected_ranges);
-        }{
-            vector<pair<int, int>> expected_ranges{{1, 2}, {1, 3}, {4, 5}, {3, 7}, {6, 9}};
-            vector<pair<int, int>> ranges{{1, 3}, {1, 2}, {3, 7}, {4, 5}, {6, 9}};
-            PrepareRanges(ranges);
-            assert(ranges == expected_ranges);
-        }
-        cerr << "TestPrepareRanges passed\n";
-    }
-
-    void TestsGetCountDisjointRanges() {
-        {
-            vector<pair<int, int>> ranges{{1, 3}, {2, 3}, {4, 5}};
-            PrepareRanges(ranges);
-            assert(GetCountDisjointRanges(ranges) == 2u);
-        }{
-            vector<pair<int, int>> ranges{{1, 2}, {2, 3}, {4, 5}, {4, 5}, {5, 6}};
-            PrepareRanges(ranges);
-            assert(GetCountDisjointRanges(ranges) == 2u);
-        }{
-            vector<pair<int, int>> ranges{{1, 50}, {49, 50}};
-            PrepareRanges(ranges);
-            assert(GetCountDisjointRanges(ranges) == 1u);
-        }
-        cerr << "TestsGetCountDisjointRanges passed\n";
-    }
-
-    void RunAllTests() {
-        TestReadRanges();
-        TestPrepareRanges();
-        TestsGetCountDisjointRanges();
-        cerr << "All tests passed\n"s;
-    }
-} // namespace tests
-
 
 int main() {
-    tests::RunAllTests();
-    vector<pair<int, int>> ranges = ReadRanges(cin);
-    PrepareRanges(ranges);
-    cout << GetCountDisjointRanges(ranges) << endl;
+    Test();
+    int n, m;
+    cin >> n >> m;
+    cout << (IsWinRocks(n, m) ? "Win" : "Loose") << endl;
     return 0;
 }
