dc8b0de0427e67013c15957f8433748ebcfe1ee4
Seredenko-V <seredenko.spb@gmail.com>
2024-02-12 16:34:15 +0300

    Разделяй и властвуй. Сортировка выбором

--- LeetCode-tasks/main.cpp
+++ LeetCode-tasks/main.cpp
@@ -1,74 +1,65 @@
 #include <iostream>
 #include <vector>
+#include <cassert>
 
 using namespace std;
 
-// Ханойские башни 2
-// https://new.contest.yandex.ru/48568/problem?id=215/2023_04_06/NQ3L2sn9W8
-// Теперь головоломка состоит из четырех стержней, а цель головоломки — перенести все диски со стержня 1 на стержень 4.
-// Найдите минимальное количество ходов, за которое можно решить головоломку.
+// Сортировка выбором
+// https://new.contest.yandex.ru/48569/problem?id=215/2023_04_06/08fmDTMXQZ
 
-// ======================================== АЛГОРИТМ РЕШЕНИЯ ========================================
-// В решении нужно правильно вычислять количество дисков, которое будет перекладываться в рамках 3-х стержней (1 буферный).
-// Оно определяется как "количество дисков / количество стержней + 1".
-// Такой подход позволит перекладывать большую часть дисков, используя 2 буферных стержня, что сократит количество операций.
+// =============================== АЛГОРИТМ РЕШЕНИЯ ================================
+// N/2 раз ищем минимальный элемент в последовательности и ставим его на i-ю позицию
 
-void SolveThreeHanoiTowers(int num_disk, int from, int to, int buf, int& count) {
-    if (num_disk < 1) {
-        return;
+template <typename Type>
+void SelectionSort(vector<Type>& vec) {
+    for (size_t i = 0; i < vec.size() - 1; ++i) {
+        size_t min_pos = i;
+        for (size_t j = i + 1; j < vec.size(); ++j) {
+            if (vec[min_pos] > vec[j]) {
+                min_pos = j;
+            }
+        }
+        swap(vec[i], vec[min_pos]);
     }
-    if (num_disk == 1) {
-        ++count;
-        return;
-    }
-    SolveThreeHanoiTowers(num_disk - 1, from, buf, to, count);
-    SolveThreeHanoiTowers(1, from, to, buf, count);
-    SolveThreeHanoiTowers(num_disk - 1, buf, to, from, count);
-}
-
-void SolveFourHanoiTowers(int num_disk, int from, int to, int first_buf, int second_buf, int& count) {
-    if (num_disk < 1) {
-        return;
-    }
-    if (num_disk == 1) {
-        ++count;
-        return;
-    }
-    static int constexpr kNumKernel = 4;
-    int num_disks_in_second_part = num_disk / kNumKernel + 1;
-    int num_disks_in_first_part = num_disk - num_disks_in_second_part - 1;
-    SolveFourHanoiTowers(num_disks_in_first_part, from, first_buf, second_buf, to, count);
-    SolveThreeHanoiTowers(num_disks_in_second_part, from, second_buf, to, count);
-    // для перемещения одного диска буфферы могут быть любыми, т.к. они не будут использоваться
-    SolveFourHanoiTowers(1, from, to, second_buf, first_buf, count);
-    SolveThreeHanoiTowers(num_disks_in_second_part, second_buf, to, first_buf, count);
-    SolveFourHanoiTowers(num_disks_in_first_part, first_buf, to, second_buf, from, count);
 }
 
 void Test() {
-    vector<int> expected_answers{0,1,3,5,9,13,17,25,33,41,49,65,81,97,113};
-    for (size_t i = 0; i < expected_answers.size(); ++i) {
-        int count = 0;
-        SolveFourHanoiTowers(i, 1, 4, 2, 3, count);
-        if (expected_answers[i] != count) {
-            cerr << count << " != "s << expected_answers[i] << '\t';
-        } else {
-            cerr << count << " == "s << expected_answers[i] << '\t';
-        }
-        cerr << i << " disks\n";
+    {
+        vector<int> sequence{13, 17, 37, 73, 31, 19, 23};
+        vector<int> expected_sequence{13, 17, 19, 23, 31, 37, 73};
+        SelectionSort(sequence);
+        assert(sequence == expected_sequence);
+    }{
+        vector<int> sequence{12, 18, 7, 11, 5, 17};
+        vector<int> expected_sequence{5, 7, 11, 12, 17, 18};
+        SelectionSort(sequence);
+        assert(sequence == expected_sequence);
+    }{
+        vector<int> sequence{1, 2, 3};
+        vector<int> expected_sequence = sequence;
+        SelectionSort(sequence);
+        assert(sequence == expected_sequence);
+    }{
+        vector<int> sequence{8};
+        vector<int> expected_sequence = sequence;
+        SelectionSort(sequence);
+        assert(sequence == expected_sequence);
     }
     cerr << "Test passed\n"s;
 }
 
 int main() {
     Test();
-    int num_disk = 0;
-    cin >> num_disk;
-    if (num_disk < 3 || num_disk > 10) {
-        return 0;
+    int size = 0;
+    cin >> size;
+    vector<int> sequence(size);
+    for (int& element : sequence) {
+        cin >> element;
     }
-    int count = 0;
-    SolveFourHanoiTowers(num_disk, 1, 4, 2, 3, count);
-    cout << count << endl;
+    SelectionSort(sequence);
+    for (int element : sequence) {
+        cout << element << ' ';
+    }
+    cout << endl;
     return 0;
 }
