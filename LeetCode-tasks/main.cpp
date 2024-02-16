53fea523525043efb2f0950377dac0ed35fab266
Seredenko-V <seredenko.spb@gmail.com>
2024-02-16 13:01:27 +0300

    Рандомизированные алгоритмы [2/2]. Опорный элемент выбирается случайно. Работает для итераторов произвольного доступа

--- LeetCode-tasks/main.cpp
+++ LeetCode-tasks/main.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 #include <vector>
 #include <cassert>
+#include <random>
 
 using namespace std;
 
 // Быстрая сортировка.
 // https://new.contest.yandex.ru/48570/problem?id=215/2023_04_06/qQS9a7rB0y
 
-// Опорный элемент всегда последний. Решение не проходит по скорости
+// Опорный элемент выбирается случайно. Решение для Random Access итераторов
 
 ostream& operator<<(ostream& out, const vector<int>& vec) {
     for (int element : vec) {
@@ -16,36 +17,72 @@ ostream& operator<<(ostream& out, const vector<int>& vec) {
     return out;
 }
 
+int GetRandomValue(int left_bound, int right_bound) {
+    static random_device rd;
+    static mt19937 gen(rd());
+    uniform_int_distribution<> distrib(left_bound, right_bound);
+    return distrib(gen);
+}
+
+template <typename Iterator>
+Iterator FindBound(Iterator begin, Iterator end, size_t size) {
+    using Type = typename iterator_traits<Iterator>::value_type;
+
+    Iterator it_pivot = begin + GetRandomValue(0, size);
+    const Type kPivot = *it_pivot;
+    Iterator left_ptr = begin;
+    Iterator right_ptr = prev(end);
+
+    while (left_ptr <= right_ptr) {
+        while (*left_ptr < kPivot) {
+            ++left_ptr;
+        }
+        while (*right_ptr > kPivot) {
+            --right_ptr;
+        }
+        if (left_ptr <= right_ptr) {
+            swap(*left_ptr, *right_ptr);
+            // чтобы не выйти за пределы диапазона
+            if (left_ptr != prev(end)) {
+                ++left_ptr;
+            }
+            if (right_ptr != begin) {
+                --right_ptr;
+            }
+        }
+    }
+    return left_ptr;
+}
+
 template <typename Iterator>
 void QuickSort(Iterator begin, Iterator end) {
-    using Type = typename iterator_traits<Iterator>::value_type;
     const size_t kSizeRange = distance(begin, end);
     if (kSizeRange < 2) {
         return;
     }
-
-    Iterator it_pivot = prev(end);
-    const Type kPivot = *it_pivot;
-    Iterator it = begin;
-    while (it != prev(it_pivot)) {
-        if (*it > kPivot) {
-            swap(*it_pivot, *prev(it_pivot));
-            swap(*it, *(it_pivot--));
-        } else {
-            ++it;
-        }
-    }
-    // чтобы не менять дважды местами одни и те же числа
-    if (*it > kPivot) {
-        swap(*it, *(it_pivot--));
-    }
-    QuickSort(begin, it_pivot);
-    QuickSort(next(it_pivot), end);
+    Iterator bound = FindBound(begin, end, kSizeRange);
+    QuickSort(begin, bound); // [begin, bound)
+    QuickSort(bound, end); // [bound, begin)
 }
 
 namespace tests {
     void TestQuickSort() {
         {
+            vector<int> seq{2,1};
+            const vector<int> kExpectedSeq{1,2};
+            QuickSort(seq.begin(), seq.end());
+            assert(seq == kExpectedSeq);
+        }{
+            vector<int> seq{3,1,2};
+            const vector<int> kExpectedSeq{1,2,3};
+            QuickSort(seq.begin(), seq.end());
+            assert(seq == kExpectedSeq);
+        }{
+            vector<int> seq{3,9,7,8,10,27};
+            const vector<int> kExpectedSeq{3,7,8,9,10,27};
+            QuickSort(seq.begin(), seq.end());
+            assert(seq == kExpectedSeq);
+        }{
             vector<int> seq{2,12,5,48,0,4};
             const vector<int> kExpectedSeq{0,2,4,5,12,48};
             QuickSort(seq.begin(), seq.end());
@@ -86,7 +123,6 @@ namespace tests {
 
 
 int main() {
-    ctime(NULL);
     tests::TestQuickSort();
     int size = 0;
     cin >> size;
