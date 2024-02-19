e3063b87a8f315b56fec56963a517e99ef37f38e
Seredenko-V <seredenko.spb@gmail.com>
2024-02-19 16:37:38 +0300

    Сумма двух чисел [3/4]. Кривая сумма строк

--- LeetCode-tasks/main.cpp
+++ LeetCode-tasks/main.cpp
@@ -1,101 +1,77 @@
 #include <iostream>
-#include <vector>
 #include <cassert>
-#include <cstdint>
+#include <string>
 
 using namespace std;
 
-// A(x) + B(x).
-// https://new.contest.yandex.ru/42492/problem?id=215/2022_11_08/r1Y2AFKOd5
+// A+B строки.
+// https://new.contest.yandex.ru/42492/problem?id=6789665/2023_04_06/GWcZqPTpiJ
 
-// Вычислить сумму многочленов
-// A(x) = a_n * x^n + ... + a_1 * x + a_0
-// B(x) = b_m * x^m + ... + b_1 * x + b_0
+// Вычислить "кривую" сумму двух строк A и B одинаковой длины.
+// "Кривой" суммой двух строк называется операция следующего вида:
+// C = A + B = A1B1A2B2...AnBn
 
-template <typename T>
-ostream& operator<<(ostream& out, const vector<T>& vec) {
-    for (T element : vec) {
-        out << element << ' ';
-    }
-    return out;
-}
 
-template <typename T>
-istream& operator>>(istream& in, vector<T>& vec) {
-    size_t size = 0;
-    in >> size;
-    vec.resize(size + 1); // с учетом 0-й степени
-    for (T& element : vec) {
-        in >> element;
+istream& ReadString(istream& in, string& str) {
+    for (char& symbol : str) {
+        in >> symbol;
     }
     return in;
 }
 
-template <typename T>
-vector<T> PolynomialSum(const vector<T>& lhs, const vector<T>& rhs) {
-    assert(lhs.size() >= rhs.size());
-    vector<T> polynom_sum = lhs;
-    for (size_t i = lhs.size() - rhs.size(), j = 0; i < lhs.size(); ++i, ++j) {
-        polynom_sum[i] += rhs[j];
+string SumCurve(const string& lhs, const string& rhs) {
+    if (lhs.size() != rhs.size()) {
+        return {};
     }
-    return polynom_sum;
-}
-
-template <typename T>
-vector<T> operator+(const vector<T>& lhs, const vector<T>& rhs) {
-    return lhs.size() > rhs.size() ? PolynomialSum(lhs, rhs) : PolynomialSum(rhs, lhs);
+    string result_sum(lhs.size() * 2, '#');
+    // можно ввести дополнительную переменную внутри цикла, чтобы не умножать i
+    for (size_t i = 0; i < lhs.size(); ++i) {
+        result_sum[i * 2] = lhs[i];
+        result_sum[i * 2 + 1] = rhs[i];
+    }
+    return result_sum;
 }
 
 
 namespace tests {
-    void TestPolynomialSum() {
+    void TestSumCurve() {
         {
-            vector<int> A{1,2,3,4};
-            vector<int> B{1,0,0};
-            vector<int> expected_sum{1,3,3,4};
-            assert(A + B == expected_sum);
+            const string kLhs = "abc"s;
+            const string kRhs = "def"s;
+            const string kExpected = "adbecf"s;
+            assert(kExpected == SumCurve(kLhs, kRhs));
         }{
-            vector<int> A{1};
-            vector<int> B{1,2,3,4,5,6,7,8,9,0};
-            vector<int> expected_sum{1,2,3,4,5,6,7,8,9,1};
-            assert(A + B == expected_sum);
+            const string kLhs = "abaca"s;
+            const string kRhs = "bdaef"s;
+            const string kExpected = "abbdaaceaf"s;
+            assert(kExpected == SumCurve(kLhs, kRhs));
         }{
-            vector<int> A{1,1};
-            vector<int> B = A;
-            vector<int> expected_sum{2,2};
-            assert(A + B == expected_sum);
-        }{
-            vector<int> A{-100};
-            vector<int> B = A;
-            vector<int> expected_sum{-200};
-            assert(A + B == expected_sum);
-        }{
-            vector<int> A{-100, 100};
-            vector<int> B{100, -150};
-            vector<int> expected_sum{0, -50};
-            assert(A + B == expected_sum);
-        }{
-            vector<int> A;
-            vector<int> B;
-            assert((A + B).empty());
-        }{
-            vector<int> A;
-            vector<int> B{1,2,4,6,7,9};
-            assert(A + B == B);
+            const string kLhs = "y"s;
+            const string kRhs = "z"s;
+            const string kExpected = "yz"s;
+            assert(kExpected == SumCurve(kLhs, kRhs));
+        }{ // разные размеры
+            const string kLhs = "yfasgfag"s;
+            const string kRhs = "z"s;
+            assert(SumCurve(kLhs, kRhs).empty());
+        }{ // пустые строки
+            const string kLhs;
+            const string kRhs;
+            assert(SumCurve(kLhs, kRhs).empty());
         }
-        cerr << "TestPolynomialSum passed"s << endl;
+        cerr << "TestSumCurve passed"s << endl;
     }
 } // namespace tests
 
 
 int main() {
-    tests::TestPolynomialSum();
-    vector<int> A;
-    vector<int> B;
-    cin >> A >> B;
-    vector<int> C = A + B;
-    !C.size() ? cout << 0 : cout << C.size() - 1;
-    cout << endl;
-    cout << C << endl;
+    tests::TestSumCurve();
+    int size = 0;
+    cin >> size;
+    string A(size, '#');
+    string B(size, '#');
+    ReadString(cin, A);
+    ReadString(cin, B);
+    cout << SumCurve(A, B) << endl;
     return 0;
 }
